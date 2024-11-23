b6643f490b46871623abe66229e142d0b5ddca0a
aditya_tewary <aditya_tewary@epam.com>
2024-11-23 13:01:22 +0530

    opitmize site_geo dao final_all()

--- redisolar/dao/redis/site_geo.py
+++ redisolar/dao/redis/site_geo.py
@@ -91,9 +91,13 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
         site_ids = self.redis.zrange(self.key_schema.site_geo_key(), 0, -1)
         sites = set()
 
+        pipeline = self.redis.pipeline()
         for site_id in site_ids:
             key = self.key_schema.site_hash_key(site_id)
-            site_hash = self.redis.hgetall(key)
+            pipeline.hgetall(key)
+
+        site_hashes = pipeline.execute()
+        for site_hash in site_hashes:
             sites.add(FlatSiteSchema().load(site_hash))
 
         return sites
